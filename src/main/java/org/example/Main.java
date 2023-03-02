e23c51762cc3aac2e82bff54e5d5d4d665c87fd0
HoeSeong123 <ghltjd3@gmail.com>
2023-03-02 13:50:49 +0900

    9단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,58 +1,108 @@
 package org.example;
 
+import java.io.*;
 import java.util.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException, RuntimeException {
         Scanner sc = new Scanner(System.in);
-        int num = 0;
-        HashMap<Integer, WiseSaying> wiseSayings = new HashMap<>();
-       // List<WiseSaying> wiseSayings = new ArrayList<>();
+
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject;
+
+        // JSON 파일 읽기
+        Reader reader = new FileReader("src/main/java/org/example/data.json");
+        try {
+            jsonObject = (JSONObject) parser.parse(reader);
+        } catch(Exception e) {
+            jsonObject = new JSONObject();
+        }
+
+        long num;
+        try {
+            num = (long) jsonObject.get("num");
+        } catch(NullPointerException e) {
+            num = 0;
+        }
+        // List<WiseSaying> wiseSayings = new ArrayList<>();
 
         System.out.println("== 명언 앱 ==");
 
         while(true) {
             System.out.print("명령) ");
-
             String command = sc.nextLine();
 
+
             if (command.equals("등록")) {
+                JSONObject jObject = new JSONObject();
                 num++;
-                WiseSaying wiseSaying = new WiseSaying();
 
                 System.out.print("명언 : ");
-                wiseSaying.setContent(sc.nextLine());
+                jObject.put("content", sc.nextLine());
 
                 System.out.print("작가 : ");
-                wiseSaying.setAuthor(sc.nextLine());
+                jObject.put("author", sc.nextLine());
 
                 System.out.println(num + "번 명언이 등록되었습니다.");
 
-                wiseSayings.put(num, wiseSaying);
+                jsonObject.put(Long.toString(num), jObject);
             }
 
             if (command.equals("목록")) {
-                List<Integer> keySet = new ArrayList<>(wiseSayings.keySet());
+                List<String> keySet = new ArrayList<>(jsonObject.keySet());
                 Collections.reverse(keySet);
 
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
 
-                for(Integer key : keySet) {
-                    System.out.println(key + " / " + wiseSayings.get(key).getAuthor() + " / " +  wiseSayings.get(key).getContent());
+                for(String key : keySet) {
+                    if(key.equals("num")) continue;
+                    System.out.println(key + " / " + ((HashMap)jsonObject.get(key)).get("author") + " / " + ((HashMap)jsonObject.get(key)).get("content"));
                 }
             }
 
             if (command.contains("삭제?id=")) {
-                int id = Integer.parseInt(command.split("=")[1]);
-                if(wiseSayings.containsKey(id) != true) {
+                String id = command.split("=")[1];
+
+                if(jsonObject.containsKey(id) != true) {
                     System.out.println(id + "번 명령이 존재하지 않습니다.");
                 } else {
                     System.out.println(id + "번 명언이 삭제되었습니다.");
-                    wiseSayings.remove(id);
+                    jsonObject.remove(id);
                 }
             }
+
+            if (command.contains("수정?id=")) {
+                String id = command.split("=")[1];
+                JSONObject jObject = new JSONObject();
+
+                System.out.println("명언(기존) : " + ((HashMap)jsonObject.get(id)).get("content"));
+                System.out.print("명언 : ");
+                jObject.put("content", sc.nextLine());
+
+                System.out.println("작가(기존) : " +((HashMap)jsonObject.get(id)).get("author"));
+                System.out.print("작가 : ");
+                jObject.put("author", sc.nextLine());
+
+                jsonObject.put(id, jObject);
+            }
+
             if (command.equals("종료")) {
+                jsonObject.put("num", num);
+                try {
+                    FileWriter file = new FileWriter("src/main/java/org/example/data.json");
+                    file.write(jsonObject.toString());
+                    file.flush();
+                    file.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             }
         }
