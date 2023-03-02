18548278c73c0c8cdef832925370f89b6d698ace
HoeSeong123 <ghltjd3@gmail.com>
2023-03-02 17:32:43 +0900

    강사님 코드 따라해보기 2단계

---
+++ src/main/java/org/answer_by_teacher/Main.java
@@ -0,0 +1,13 @@
+package org.answer_by_teacher;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
+    }
+}
