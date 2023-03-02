627aad6e7c9cec0ee1798052205fab28c054c0ee
HoeSeong123 <ghltjd3@gmail.com>
2023-03-02 09:29:07 +0900

    2단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String command = sc.nextLine();
+        while(true) {
+            System.out.print("명령) ");
+
+            String command = sc.nextLine();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+            }
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
     }
 }
