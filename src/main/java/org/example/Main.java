0a40773477e1268ecc77db90bc6116ad8b427d16
HoeSeong123 <ghltjd3@gmail.com>
2023-03-02 09:34:38 +0900

    4단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-
+        int num = 1;
         while(true) {
             System.out.print("명령) ");
 
@@ -17,7 +17,8 @@ public class Main {
                 String wiseSaying = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(num + "번 명언이 등록되었습니다.");
+                num++;
             }
 
             if (command.equals("종료")) {
